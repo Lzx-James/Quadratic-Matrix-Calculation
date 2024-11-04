5548053a17d73bc2462a010222053d1faedf2f27
James <3074393135@qq.com>
2024-11-03 18:38:25 -0800

    Add ellipse plot code. Ellipse parameters are pre-calculated

--- 2d-gaussian.py
+++ 2d-gaussian.py
@@ -1,9 +1,12 @@
 # Importing the necessary modules
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.patches import Ellipse
+from scipy.linalg import sqrtm
 from scipy.stats import multivariate_normal
 
 plt.rcParams['figure.figsize'] = 8, 8
+
 
 def generate_and_plot(kx, mu):
     distr = multivariate_normal(cov=kx, mean=mu, seed=1000)
@@ -19,9 +22,26 @@ def generate_and_plot(kx, mu):
 
 # Define the mean and covariance matrix
 Kx = np.array([[2.0, 1.0],
-               [1.0, 4.0]])
-mu = np.array([0, 0])
+               [1.0, 2.0]])
+mu = np.array([1, 2])
 random_seed = 10
 
 generate_and_plot(Kx, mu)
+
+a = np.sqrt(1)  # Principle axes
+b = np.sqrt(3)
+theta = np.radians(-45)
+
+# 生成椭圆的点
+t = np.linspace(0, 2 * np.pi, 100)
+x = a * np.cos(t)
+y = b * np.sin(t)
+
+# 旋转椭圆
+x_rotated = x * np.cos(theta) - y * np.sin(theta) + mu[0]
+y_rotated = x * np.sin(theta) + y * np.cos(theta) + mu[1]
+
+# 绘制
+plt.plot(x_rotated, y_rotated, label='斜椭圆', color='red')
+
 plt.show()
